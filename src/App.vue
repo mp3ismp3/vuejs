8023a88d5815df30098f62877a7c9f2ca9c78fea
Yeng Ping <peter2mary@gmail.com>
2020-10-05 09:57:43 +0800

    remove page order

--- src/App.vue
+++ src/App.vue
@@ -54,6 +54,25 @@ export default {
     let apiUrl = 'http://127.0.0.1:8000/api/task-list/'
     axios.get(apiUrl).then(res=>{
       this.portfolios = res.data
+    }).catch(err=>{
+      if (err && err.response) {
+      switch (err.response.status) {
+        case 404:
+          console.log("找不到該頁面");
+          break;
+        case 500:
+          console.log("伺服器出錯");
+          break;
+        case 503:
+          console.log("服務失效");
+          break;
+        default:
+          console.log('連接錯誤${err.response.status}');
+      }
+    } else {
+      console.log("連接到服務器失敗");
+    }
+    return Promise.resolve(err.response);
     })
   },
   computed:{
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import PageOrder from '@/components/pages/PageOrder'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/PageOrder',
-      name: 'PageOrder',
-      component: PageOrder
-    },
+
   ]
 })
